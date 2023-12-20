c11018c38480dc96a5e755a2ef02a18e877de908
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-20 10:20:58 -0300

    10° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/bread_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Bread, type: :model do
+
+  before{@bread = Bread.new}
+
+  describe 'Testes de preenchimento do model Bread' do
+
+    it 'name consegue ser preenchido?' do
+      @bread.name = ''
+      expect(@bread.name).to eq('pão de forma')
+    end
+
+  end
+
+end
